f7887747b1167da172d7c33cebd14768df9ad98f
elena7130 <qudongqin20@163.com>
2024-05-27 11:04:12 +0800

    增加 Canonical

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import GoogleAnalytics from '../components/GoogleAnalytics'
+import GoogleAnalytics from '../components/GoogleAnalytics';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const canonicalUrl = (`https://dragon-coloringpages.com` + (router.asPath === "/" ? "" : router.asPath)).split("?")[0];
+
+
   return (
   <>
   <GoogleAnalytics />
+  <Head>
+        <link rel="canonical" href={canonicalUrl} />
+  </Head>
   <Component {...pageProps} />;
   </>
     )
