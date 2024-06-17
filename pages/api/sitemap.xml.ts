c4297c57e89e480721f105139b47ffb2680386d2
elena7130 <qudongqin20@163.com>
2024-06-17 12:28:08 +0800

    更新站点地图

--- pages/api/sitemap.xml.ts
+++ pages/api/sitemap.xml.ts
@@ -1,6 +1,6 @@
-// pages/api/sitemap.xml.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllPosts } from '../../utils/getAllPosts';
+import pool from '../../lib/db';
 
 interface Post {
   slug: string;
@@ -10,23 +10,63 @@ interface Post {
   };
 }
 
+interface ImageData {
+  id: string;
+  url: string;
+  description: string;
+  created_at: string;
+}
+
+const additionalPages = [
+  { url: 'https://www.dragon-coloringpages.com/', lastmod: new Date().toISOString(), priority: 1.0 },
+  { url: 'https://www.dragon-coloringpages.com/sign-in', lastmod: new Date().toISOString(), priority: 0.8 },
+  { url: 'https://www.dragon-coloringpages.com/generate', lastmod: new Date().toISOString(), priority: 0.8 },
+  { url: 'https://www.dragon-coloringpages.com/gallery', lastmod: new Date().toISOString(), priority: 0.8 },
+];
+
+async function getGalleryImages() {
+  const client = await pool.connect();
+  const res = await client.query('SELECT id, url, created_at FROM images');
+  const images: ImageData[] = res.rows.map((image: any) => ({
+    ...image,
+    created_at: new Date(image.created_at).toISOString(),
+  }));
+  client.release();
+  return images;
+}
+
 export default async function sitemapXml(req: NextApiRequest, res: NextApiResponse) {
   const posts: Post[] = getAllPosts();
   const environments = {
     development: 'http://localhost:3000',
     production: 'https://www.dragon-coloringpages.com',
-    test: 'http://localhost:3000' // åè®¾ä½ æä¸ä¸ªåä¸º'test'çç¯å¢
+    test: 'http://localhost:3000'
   };
   const environment = process.env.NODE_ENV as keyof typeof environments;
   const baseUrl = environments[environment] || 'https://www.dragon-coloringpages.com';
 
+  const postUrls = posts.map(post => ({
+    url: `${baseUrl}/${post.slug}`,
+    lastmod: new Date(post.frontMatter.date).toISOString(),
+    priority: 0.7,
+  }));
+
+  const galleryImages = await getGalleryImages();
+  const galleryUrls = galleryImages.map(image => ({
+    url: `${baseUrl}/images/${image.id}`,
+    lastmod: image.created_at,
+    priority: 0.5,
+  }));
+
+  const allUrls = [...postUrls, ...additionalPages, ...galleryUrls];
+
   const sitemap: string = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${posts.map(post => `
+${allUrls.map(({ url, lastmod, priority }) => `
   <url>
-    <loc>${baseUrl}/${post.slug}</loc>
-    <lastmod>${new Date(post.frontMatter.date).toISOString()}</lastmod>
-    <priority>0.7</priority>
+    <loc>${url}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <priority>${priority}</priority>
   </url>
 `).join('')}
 </urlset>`;
